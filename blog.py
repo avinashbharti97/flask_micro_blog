baa89b844f19d3bbafd93dfff250d3d8d9162949
avinashbharti97 <avinashbharti97@gmail.com>
2018-02-10 02:57:02 +0530

    login system created

--- blog.py
+++ blog.py
@@ -7,6 +7,10 @@ import sqlite3
 
 #configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\xfas\x1c\xc0\xae\x1d\x95 \x06\xe7\xa7\nn\x1e\xb2\xfc&\xcb\xebZ\xdd\xec)\xbe'
+
 
 
 app = Flask(__name__)
@@ -18,12 +22,28 @@ app.config.from_object(__name__)
 def coonect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please Try again'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error = error), status_code
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('you were logged out')
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     app.run(debug=True)
